6260c6331bac2179b139d8c13011bccfe19fd7d9
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-05-24 21:15:34 +0300

    Done source

--- lab04/mainwindow.cpp
+++ lab04/mainwindow.cpp
@@ -27,6 +27,15 @@ MainWindow::~MainWindow() {
 	delete ui;
 }
 
+static QPen getPen(int i) {
+	const auto r = static_cast<int>(qSin(i * 0.3)       * 100) + 100;
+	const auto g = static_cast<int>(qSin(i * 0.6 + 0.7) * 100) + 100;
+	const auto b = static_cast<int>(qSin(i * 0.4 + 0.6) * 100) + 100;
+	QPen pen(QColor(r, g, b));
+	pen.setWidth(2);
+	return pen;
+}
+
 void MainWindow::on_calculatePushButton_clicked() {
 	const Parameters parameters = {
 		ui->F0_doubleSpinBox->value(),
@@ -34,25 +43,21 @@ void MainWindow::on_calculatePushButton_clicked() {
 
 	auto result = solve(parameters);
 
-	ui->Tx_customPlot->clearGraphs();
+	for (auto&& customPlot: customPlots) {
+		customPlot->clearGraphs();
+	}
+
 	for (int i = 0, j = 0; i < result.Tx.size(); i += 3, ++j) {
-		auto label = QString("time = ") + QString::number(result.t[i]);
 		ui->Tx_customPlot->addGraph();
-		QPen pen(QColor(int(qSin(j*0.3)*100)+100, int(qSin(j*0.6+0.7)*100)+100, int(qSin(j*0.4+0.6)*100)+100));
-		pen.setWidth(2);
-		ui->Tx_customPlot->graph()->setPen(pen);
-		ui->Tx_customPlot->graph()->setName(label);
+		ui->Tx_customPlot->graph()->setPen(getPen(j));
+		ui->Tx_customPlot->graph()->setName(QString("time = ") + QString::number(result.t[i]));
 		ui->Tx_customPlot->graph()->setData(result.x, result.Tx[i]);
 	}
 
-	ui->Tt_customPlot->clearGraphs();
 	for (int i = 0; i < result.Tt.size(); ++i) {
-		auto label = QString("len = ") + QString::number(i);
 		ui->Tt_customPlot->addGraph();
-		QPen pen(QColor(int(qSin(i*0.3)*100)+100, int(qSin(i*0.6+0.7)*100)+100, int(qSin(i*0.4+0.6)*100)+100));
-		pen.setWidth(2);
-		ui->Tt_customPlot->graph()->setPen(pen);
-		ui->Tt_customPlot->graph()->setName(label);
+		ui->Tt_customPlot->graph()->setPen(getPen(i));
+		ui->Tt_customPlot->graph()->setName(QString("len = ") + QString::number(i));
 		ui->Tt_customPlot->graph()->setData(result.t, result.Tt[i]);
 	}
 
